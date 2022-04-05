3dc6970be22bd9a4bffc6aea1a7845be41bfe05d
leoleoleo17 <67807484+leoleoleo17@users.noreply.github.com>
2022-04-05 18:47:13 -0500

    Update myBuffer.hpp

--- myBuffer.hpp
+++ myBuffer.hpp
@@ -1,1 +1,26 @@
-#pragma once
+#ifndef _MYBUFFER_HPP
+#define _MYBUFFER_HPP
+
+#include <string>
+#include <iostream>
+#include <stdexcept>
+#include <stack>
+
+class EditorBuffer{
+    private:
+        std::stack<char> before;
+        std::stack<char> after;
+    public:
+        EditorBuffer();
+        ~EditorBuffer();
+        void moveCursorForward();
+        void moveCursorBackward();
+        void moveCursorToStart();
+        void moveCursorToEnd();
+        void insertCharacter(char ch);
+        void deleteCharacter();
+        std::string getText() const;
+        unsigned getCursor() const;
+        void display() const;
+};
+#endif
